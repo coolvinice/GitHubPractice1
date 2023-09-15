8e43aa1fde6efd85e24f082ec18d113ed5f484f2
Calvin Rice <ramen.calvin@yahoo.com>
2023-09-15 11:00:15 -0500

    Initial Commit

---
+++ GitHubPractice1/GitHubPractice1App.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubPractice1App.swift
+//  GitHubPractice1
+//
+//  Created by Calvin Rice on 9/15/23.
+//
+
+import SwiftUI
+
+@main
+struct GitHubPractice1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
